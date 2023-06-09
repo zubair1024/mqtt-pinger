af4490a02bee1d768fb363d3e315aa0e619e2001
Zubair Ahmed <zubair1024@gmail.com>
2023-06-09 21:34:32 +0200

    init commit

---
+++ apps/fe/pages/dashboard-screen/index.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface DashboardScreenProps {}
+
+export function DashboardScreen(props: DashboardScreenProps) {
+  return (
+    <div>
+      <h1>Welcome to DashboardScreen!</h1>
+    </div>
+  );
+}
+
+export default DashboardScreen;
